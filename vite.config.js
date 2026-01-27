4e0acfde83fec1215e01f70f10cfde901a29a4a7
Marcus <clawpn@gmail.com>
2026-01-27 21:52:46 +0300

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,4 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     base: '/clawpn/',
     plugins: [vue()],
-    
-    build: {
-        outDir: 'dist',
-        assetsDir: '',
-        emptyOutDir: true,
-        
-        rollupOptions: {
-            output: {
-                assetFileNames: '[name]-[hash][extname]',
-                chunkFileNames: '[name]-[hash].js',
-                entryFileNames: '[name]-[hash].js'
-            }
-        }
-    }
 })
